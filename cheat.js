f22c93bc9fe87b4a7b95ee6963048b7022be6066
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-12-15 21:51:57 +0700

    Create cheat.js

---
+++ cheat.js
@@ -0,0 +1,7 @@
+Simplify.brawlstars.generator
+if true [("999999 Gems")]
+if true [("999999 Koin")]
+if true [("93618 Trofi")]
+not.detection/~"hack.detection if false"
+Simplify.999999Gems"[{"masukkan/+Kedalam/+https://brawlstars.com/chaet"}]
+Simllify.999999Koin"[{"masukkan/-Kedalam/-https://cloudflare.com/hack-system.js"}]
